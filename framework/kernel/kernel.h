d421640146289f2c99747e6f786a6b33b7ab0c8e
Debabrata Ghosh <rijugh@gmail.com>
2024-01-08 08:39:27 +0100

    With added cohesion and adhesion kernels

--- framework/kernel/kernel.h
+++ framework/kernel/kernel.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cassert>
 #include <Eigen/Dense>
+#include <cmath>
 
 namespace kernel
 {
@@ -12,12 +13,18 @@ namespace kernel
 
 		double cubic_spline(const Eigen::Vector3d x_i, const Eigen::Vector3d x_j);
 		Eigen::Vector3d cubic_grad_spline(const Eigen::Vector3d x_i, const Eigen::Vector3d x_j);
+		double cohesion_kernel(const Eigen::Vector3d x_i, const Eigen::Vector3d x_j);
+		double adhesion_kernel(const Eigen::Vector3d x_i, const Eigen::Vector3d x_j);
 
 	private:
 		double smoothing_length;
 		double inverse_h;
 		double inverse_h3;
 		double inverse_h4;
-
+		double compact_support;
+		double c3;
+		double c6;
+		double inverse_c9;
+		double inverse_c325;
 	};
 };
